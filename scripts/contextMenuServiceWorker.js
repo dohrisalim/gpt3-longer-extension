6f25bc88b5e5fdcb27aecf5ad4691fcb3b96eb83
Salim Dohri <dohri.salim@gmail.com>
2023-02-05 13:40:06 -0500

    latest build

---
+++ scripts/contextMenuServiceWorker.js
@@ -0,0 +1,80 @@
+const getKey = () => {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get(['openai-key'], (result) => {
+        if (result['openai-key']) {
+          const decodedKey = atob(result['openai-key']);
+          resolve(decodedKey);
+        }
+      });
+    });
+  };
+
+const sendMessage = (content) => {
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTab = tabs[0].id;
+
+    chrome.tabs.sendMessage(
+    activeTab,
+    { message: 'inject', content },
+    (response) => {
+        if (response.status === 'failed') {
+        console.log('injection failed.');
+        }
+    }
+    );
+});
+};
+
+const generate = async (prompt) => {
+    // Get your API key from storage
+    const key = await getKey();
+    const url = 'https://api.openai.com/v1/completions';
+      
+    // Call completions endpoint
+    const completionResponse = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${key}`,
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: prompt,
+        max_tokens: 2250,
+        temperature: 0.7,
+      }),
+    });
+      
+    // Select the top choice and send back
+    const completion = await completionResponse.json();
+    return completion.choices.pop();
+  }
+
+const generateCompletionAction = async (info) => {
+    try {
+
+        // Send mesage with generating text (this will be like a loading indicator)
+        sendMessage('generating...');
+
+        const { selectionText } = info;
+        const baseCompletion = await generate(`${selectionText}`);
+
+        console.log(baseCompletion.text)
+        sendMessage(baseCompletion.text)
+    } catch (error) {
+      console.log(error);
+      sendMessage(error.toString());
+    }
+  };
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: 'context-run',
+      title: 'Generate ChatGPT answer',
+      contexts: ['selection'],
+    });
+  });
+  
+  // Add listener
+  chrome.contextMenus.onClicked.addListener(generateCompletionAction);
+  
